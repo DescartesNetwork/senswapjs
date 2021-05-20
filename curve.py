5441974708b52603374133b1a4b478ce827dfcf5
tuphan <tuphan@descartes.network>
2021-05-20 17:58:34 +0700

    draw curve

--- curve.py
+++ curve.py
@@ -2,6 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d
 
+density = 0.01
+ceiling = 2
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+ax.set_xlabel('A')
+ax.set_xticks([])
+ax.set_ylabel('B')
+ax.set_yticks([])
+ax.set_zlabel('SEN')
+ax.set_zticks([])
+
+
+x = np.arange(density, ceiling+density, density)
+y = np.arange(density, ceiling+density, density)
+
+
+x, y = np.meshgrid(x, y)
+z = 1/(x*y)
+
+x = x.flatten()
+y = y.flatten()
+z = z.flatten()
+mask = z <= ceiling
+x = x[mask]
+y = y[mask]
+z = z[mask]
+
+ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
+
 plt.show()
