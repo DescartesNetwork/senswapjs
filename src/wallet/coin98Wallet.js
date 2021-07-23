f431acf3d780b42191b16f4f0fbc9ecfec614338
tuphan-dn <tuphan@descartes.network>
2021-07-24 00:31:27 +0700

    support phantom

--- src/wallet/coin98Wallet.js
+++ src/wallet/coin98Wallet.js
@@ -1,65 +1,65 @@
-const bs58 = require("bs58");
+const bs58 = require('bs58')
 
-const account = require("../account");
-const storage = require("./storage");
-const WalletInterface = require("./walletInterface");
+const account = require('../account')
+const storage = require('./storage')
+const WalletInterface = require('./walletInterface')
 
 class Coin98Wallet extends WalletInterface {
   constructor() {
-    super();
+    super()
 
-    this._setWallet();
+    this._setWallet()
   }
 
   _setWallet = () => {
-    storage.set("WalletType", "Coin98");
-  };
+    storage.set('WalletType', 'Coin98')
+  }
 
   _getNode = async () => {
-    const { coin98 } = window;
-    const { sol } = coin98 || {};
-    if (!sol) throw new Error("Wallet is not connected");
-    return sol;
-  };
+    const { coin98 } = window
+    const { sol } = coin98 || {}
+    if (!sol) throw new Error('Wallet is not connected')
+    return sol
+  }
 
   _getAccount = async () => {
-    const node = await this._getNode();
-    const re = await node.request({ method: "sol_accounts" });
-    const [acc] = re || [];
-    if (!acc) throw new Error("There is no Solana account");
-    return acc;
-  };
+    const node = await this._getNode()
+    const re = await node.request({ method: 'sol_accounts' })
+    const [acc] = re || []
+    if (!acc) throw new Error('There is no Solana account')
+    return acc
+  }
 
   _sign = async (transaction) => {
-    const node = await this._getNode();
-    const acc = await this.getAccount();
-    transaction.feePayer = account.fromAddress(acc);
-    let { publicKey, signature } = await node.request({
-      method: "sol_sign",
+    const node = await this._getNode()
+    const acc = await this.getAccount()
+    transaction.feePayer = account.fromAddress(acc)
+    const { signature: sig } = await node.request({
+      method: 'sol_sign',
       params: [transaction],
-    });
-    publicKey = account.fromAddress(publicKey);
-    signature = bs58.decode(signature);
-    return { publicKey, signature };
-  };
+    })
+    const publicKey = account.fromAddress(acc)
+    const signature = bs58.decode(sig)
+    return { publicKey, signature }
+  }
 
   _certify = async (msg) => {
-    const node = await this._getNode();
-    const data = await node.request({ method: "sol_sign", params: [msg] });
-    return data;
-  };
+    const node = await this._getNode()
+    const data = await node.request({ method: 'sol_sign', params: [msg] })
+    return data
+  }
 
   _verify = async (sig, msg = null) => {
-    const addr = await this._getAccount();
-    const data = await account.verify(addr, sig, msg);
-    return data;
-  };
+    const addr = await this._getAccount()
+    const data = await account.verify(addr, sig, msg)
+    return data
+  }
 
   _disconnect = async () => {
-    const node = await this._getNode();
-    storage.clear("WalletType");
-    node.disconnect();
-  };
+    const node = await this._getNode()
+    storage.clear('WalletType')
+    node.disconnect()
+  }
 }
 
-module.exports = Coin98Wallet;
+module.exports = Coin98Wallet
--- src/wallet/phantomWallet.js
+++ src/wallet/phantomWallet.js
@@ -1,66 +1,65 @@
-const bs58 = require("bs58");
+const bs58 = require('bs58')
 
-const account = require("../account");
-const storage = require("./storage");
-const WalletInterface = require("./walletInterface");
+const account = require('../account')
+const storage = require('./storage')
+const WalletInterface = require('./walletInterface')
 
 class PhantomWallet extends WalletInterface {
   constructor() {
-    super();
+    super()
 
-    this._setWallet();
+    this._setWallet()
   }
 
   _setWallet = () => {
-    storage.set("WalletType", "Phantom");
-  };
+    storage.set('WalletType', 'Phantom')
+  }
 
   _getNode = () => {
     return new Promise((resolve, reject) => {
-      const { solana } = window;
-      if (!solana?.isPhantom) reject("Wallet is not connected");
-      if (solana.isConnected) return resolve(solana);
-      solana.connect();
-      return solana.on("connect", () => resolve(solana));
-    });
-  };
+      const { solana } = window
+      if (!solana?.isPhantom) reject('Wallet is not connected')
+      if (solana.isConnected) return resolve(solana)
+      solana.connect()
+      return solana.on('connect', () => resolve(solana))
+    })
+  }
 
   _getAccount = async () => {
-    const node = await this._getNode();
-    const acc = node.publicKey.toString();
-    if (!acc) throw new Error("There is no Solana account");
-    return acc;
-  };
+    const node = await this._getNode()
+    const acc = node.publicKey.toString()
+    if (!acc) throw new Error('There is no Solana account')
+    return acc
+  }
 
   _sign = async (transaction) => {
-    console.log("transaction", transaction);
-    const node = await this._getNode();
-    const acc = await this.getAccount();
-    transaction.feePayer = account.fromAddress(acc);
-    const { signature } = await node.signTransaction(transaction);
-    console.log("signature", signature);
-    return { publicKey: acc, signature };
-  };
+    const node = await this._getNode()
+    const acc = await this.getAccount()
+    transaction.feePayer = account.fromAddress(acc)
+    const { signature } = await node.signTransaction(transaction)
+    const publicKey = account.fromAddress(acc)
+    return { publicKey, signature }
+  }
 
   _certify = async (msg) => {
-    const node = await this._getNode();
-    const address = await this._getAccount();
-    const sig = await node.signMessage(msg, "utf8");
-    const data = { address, sig, msg };
-    return data;
-  };
+    const node = await this._getNode()
+    const address = await this._getAccount()
+    const sig = await node.signMessage(msg, 'utf8')
+    const data = { address, sig, msg }
+    return data
+  }
 
   _verify = async (sig, msg = null) => {
-    const addr = await this._getAccount();
-    const data = await account.verify(addr, sig, msg);
-    return data;
-  };
+    const addr = await this._getAccount()
+    const data = await account.verify(addr, sig, msg)
+    return data
+  }
 
   _disconnect = async () => {
-    const node = await this._getNode();
-    storage.clear("WalletType");
-    node.disconnect();
-  };
+    const node = await this._getNode()
+    storage.clear('WalletType')
+    node.disconnect()
+  }
 }
 
-module.exports = PhantomWallet;
+module.exports = PhantomWallet
