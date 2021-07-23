5f4a784297382a41f989f66fed0dee0f3a5ce492
tuphan-dn <tuphan@descartes.network>
2021-07-23 23:32:53 +0700

    support phantom

--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
-const account = require('./account');
-const defaults = require('./defaults');
-const math = require('./math');
-const keystore = require('./keystore');
-const util = require('./util');
-const schema = require('./schema');
-const Swap = require('./swap');
-const LiteSwap = require('./liteSwap');
-const SPLT = require('./splt');
-const Lamports = require('./lamports');
-const Farming = require('./farming');
-const LiteFarming = require('./liteFarming');
-const oracle = require('./oracle');
-const crypto = require('./crypto');
+const account = require("./account");
+const defaults = require("./defaults");
+const math = require("./math");
+const keystore = require("./keystore");
+const util = require("./util");
+const schema = require("./schema");
+const Swap = require("./swap");
+const LiteSwap = require("./liteSwap");
+const SPLT = require("./splt");
+const Lamports = require("./lamports");
+const Farming = require("./farming");
+const LiteFarming = require("./liteFarming");
+const oracle = require("./oracle");
+const crypto = require("./crypto");
 const {
-  WalletInterface, RawWallet,
-  SecretKeyWallet, KeystoreWallet, Coin98Wallet
-} = require('./wallet');
+  WalletInterface,
+  RawWallet,
+  SecretKeyWallet,
+  KeystoreWallet,
+  Coin98Wallet,
+  PhantomWallet,
+} = require("./wallet");
 
 module.exports = {
-  account, ...account,
-  defaults, ...defaults,
-  math, ...math,
-  keystore, ...keystore,
-  util, ...util,
-  schema, ...schema,
-  Swap, LiteSwap,
+  account,
+  ...account,
+  defaults,
+  ...defaults,
+  math,
+  ...math,
+  keystore,
+  ...keystore,
+  util,
+  ...util,
+  schema,
+  ...schema,
+  Swap,
+  LiteSwap,
   SPLT,
   Lamports,
-  Farming, LiteFarming,
-  oracle, ...oracle,
-  crypto, ...crypto,
-  WalletInterface, RawWallet,
-  SecretKeyWallet, KeystoreWallet, Coin98Wallet,
-}
+  Farming,
+  LiteFarming,
+  oracle,
+  ...oracle,
+  crypto,
+  ...crypto,
+  WalletInterface,
+  RawWallet,
+  SecretKeyWallet,
+  KeystoreWallet,
+  Coin98Wallet,
+  PhantomWallet,
+};
